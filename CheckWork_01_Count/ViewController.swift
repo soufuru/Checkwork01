ee54c6a2ee43b6571e9d53a55d8d2ab762df8ca1
soufuru <soufuru1@gmail.com>
2017-07-08 07:35:50 +0900

    Initial Commit

---
+++ CheckWork_01_Count/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CheckWork_01_Count
+//
+//  Created by SoichiFurukawa on 2017/07/08.
+//  Copyright © 2017年 soufuru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CheckWork_01_CountTests/CheckWork_01_CountTests.swift
@@ -0,0 +1,36 @@
+//
+//  CheckWork_01_CountTests.swift
+//  CheckWork_01_CountTests
+//
+//  Created by SoichiFurukawa on 2017/07/08.
+//  Copyright © 2017年 soufuru. All rights reserved.
+//
+
+import XCTest
+@testable import CheckWork_01_Count
+
+class CheckWork_01_CountTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
