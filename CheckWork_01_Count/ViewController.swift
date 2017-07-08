e9d8df36193d03702d75ed29706157d5d21cf791
soufuru <soufuru1@gmail.com>
2017-07-08 14:13:56 +0900

    commit

--- CheckWork_01_Count/ViewController.swift
+++ CheckWork_01_Count/ViewController.swift
@@ -10,9 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    /*
+        ある程度コードを書いているので、*****の部分を指示にしたがって書き換えましょう。
+        コードを書く際はコメントアウトを消すのを忘れずに！
+        (「コメントアウトを消す」とは文の頭に書いてある「//」を消すことです。
+        普通に消してもいいですが、その行で「command + /」で消すこともできます)
+    */
+    
+    // numberというInt型の変数を用意し、0を代入しましょう。
+    // var  number  : ***** = 0
+    
+    
+    //  数字を表示するためのラベルを宣言しましょう。関連付けも忘れずに。
+    //  *****  var label : UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        //labelにnumberの値を表示させましょう
+        
+        //label.***** = String(*****)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +42,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    //下記のメソッドを完成させましょう。こちらも関連付けを忘れずに！
+/*    ***** func plus() {
+    
+     //numberに1を足しましょう。
+        number = ***** + 1
+     //labelに反映させましょう。
+        label.***** = String(*****)
+     }
+*/
+    
+    
+    /*
+        どうしてもわからないところがあったら教科書を見直そう！でもできるだけ見ないように！
+        
+        ここまで完成したらマイナスボタンを作ったり、10以上になったらlabelの文字の色を変えてみよう！
+    */
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
 
