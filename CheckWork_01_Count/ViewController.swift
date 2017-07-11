48d6215aba5b76f21359e042ccf2098fc2a388de
soufuru <soufuru1@gmail.com>
2017-07-11 20:23:25 +0900

    change comment

--- CheckWork_01_Count/ViewController.swift
+++ CheckWork_01_Count/ViewController.swift
@@ -9,14 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     
     /*
-        ある程度コードを書いているので、*****の部分を指示にしたがって書き換えましょう。
-        コードを書く際はコメントアウトを消すのを忘れずに！
-        (「コメントアウトを消す」とは文の頭に書いてある「//」を消すことです。
-        普通に消してもいいですが、その行で「command + /」で消すこともできます)
-    */
+     ある程度コードを書いているので、*****の部分を指示にしたがって書き換えましょう。
+     コードを書く際はコメントアウトを消すのを忘れずに！
+     (「コメントアウトを消す」とは文の頭に書いてある「//」を消すことです。
+     普通に消してもいいですが、その行で「command + /」で消すこともできます)
+     */
     
     // numberというInt型の変数を用意し、0を代入しましょう。
     // var  number  : ***** = 0
@@ -30,37 +30,35 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
         //labelにnumberの値を表示させましょう
         
         //label.***** = String(*****)
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     
     //下記のメソッドを完成させましょう。こちらも関連付けを忘れずに！
-/*    ***** func plus() {
-    
+    /*    ***** func plus() {
+     
      //numberに1を足しましょう。
-        number = ***** + 1
+     number = ***** + 1
      //labelに反映させましょう。
-        label.***** = String(*****)
+     label.***** = String(*****)
      }
-*/
+     */
     
     
-    /*
-        どうしてもわからないところがあったら教科書を見直そう！でもできるだけ見ないように！
-        
-        ここまで完成したらマイナスボタンを作ったり、10以上になったらlabelの文字の色を変えてみよう！
-    */
     
     
+    
+    
+    //    ここまで完成したらマイナスボタンを作ったり、10以上になったらlabelの文字の色を変えてみよう！
     
     
     
@@ -72,6 +70,5 @@ class ViewController: UIViewController {
     
     
     
-
 }
 
